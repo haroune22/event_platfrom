2f88835e64c82d613df672948f9edb315130640a
haroune22 <harounehamenniche123@gmail.com>
2026-05-06 11:52:51 +0100

    auth finished

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { Login } from "../controllers/userController.js";
+import { Login, Register } from "../controllers/userController.js";
 
 const router = Router()
 
 
-router.get('/login', Login)
+router.post('/login', Login)
+router.post('/register', Register)
 
 
 export default router
